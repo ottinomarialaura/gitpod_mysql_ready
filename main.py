9e8b8d2d8efae948ac0d057d7fa4f4a788213eba
ottinomarialaura <ottino.marialaura@iisgalvanimi.edu.it>
2023-03-01 11:54:05 +0000

    fine

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 mycursor.execute("CREATE DATABASE animali")
+mycursor.execute("CREATE TABLE customers (ID INT(50), name_proprio VARCHAR(255), razza VARCHAR(255), peso INT(10), eta INT(5))")
