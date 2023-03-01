deafe65ca51a643bbaef9dd4356431c10dbaf3ae
ottinomarialaura <ottino.marialaura@iisgalvanimi.edu.it>
2023-03-01 11:56:19 +0000

    fine

---
+++ prep.B.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (ID INT(50), name_proprio VARCHAR(255), razza VARCHAR(255), peso INT(10), eta INT(5))")
