15c23e3e5e1b0687173465848cf005fe2d8c8601
ottinomarialaura <ottino.marialaura@iisgalvanimi.edu.it>
2023-03-08 11:58:22 +0000

    donne

--- prep.B.py
+++ prep.B.py
@@ -3,9 +3,22 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+  database="animali"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE customers (ID INT(50), name_proprio VARCHAR(255), razza VARCHAR(255), peso INT(10), eta INT(5))")
+sql = "INSERT INTO mammiferi (id, name_proprio, razza, peso, eta) VALUES (%s, %s, %s, %s, %s)"
+val =[
+ ("2", "gatto", "certosino", "5", "15"),
+ ("3", "cane", "barboncino", "3", "11"),
+ ("4", "topo", "grigio", "1", "1"),
+ ("5", "giraffa", "africana", "300", "2"),
+ ("1", "elefante", "indiano", "600", "4")
+]
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
